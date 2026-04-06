e42a9e9aa5624e470be14e9a13a566e9e4f08eb3
jegyes <jegyes@madisoncollege.edu>
2026-04-05 23:13:58 -0500

    initialized theme icons

--- src/App.tsx
+++ src/App.tsx
@@ -145,6 +145,11 @@ const App = () => {
           {/* this hidden checkbox controls the state */}
           <input 
             type="checkbox"
+            checked=
+              {
+                "garden" !== theme
+              }
+
             onClick={
               async () => {
                 const result = await localforage.setItem<string>(
@@ -163,7 +168,7 @@ const App = () => {
 
           {/* moon icon */}
           <svg
-            className="swap-on h-10 w-10 fill-current"
+            className="swap-off h-10 w-10 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24">
             <path
@@ -172,7 +177,7 @@ const App = () => {
 
           {/* sun icon */}
           <svg
-            className="swap-off h-10 w-10 fill-current"
+            className="swap-on h-10 w-10 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24">
             <path
